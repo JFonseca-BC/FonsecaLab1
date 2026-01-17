0a3cdf9079fa1831c2a8ac7e0d98550bf50868ad
JFonseca-BC <jfonseca10@my.bcit.ca>
2026-01-17 13:24:39 -0800

    Added UI text to user.js

---
+++ lang/messages/en/user.js
@@ -0,0 +1,31 @@
+const indexPage = "index.html";
+const writerPage = "writer.html";
+const readerPage = "reader.html";
+
+const pageTitles = [
+  indexPage,
+  writerPage,
+  readerPage,
+];
+
+const updated = "Updated at:";
+const stored = "Stored at:";
+
+const timeTexts = [
+    updated,
+    stored,
+];
+
+const addButton = "add";
+const removeButton = "remove";
+
+const buttons = [
+    addButton, 
+    removeButton,
+];
+
+window.userText = {
+    pageTitles,
+    timeTexts,
+    buttons,
+};
